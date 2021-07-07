7bfddc8e73ac5252fca8774bf14d62796f91ffb9
lukemartinlogan <lukemartinlogan@gmail.com>
2021-07-07 10:32:31 -0500

    Readme

--- include/pmemcpy/serialize/serializer_factory.h
+++ include/pmemcpy/serialize/serializer_factory.h
@@ -1,15 +1,16 @@
 //
 // Created by lukemartinlogan on 5/28/21.
 //
+// NOTE: Disabling msgpack, cereal, and boost
 
 #ifndef PMEMCPY_SERIALIZER_FACTORY_H
 #define PMEMCPY_SERIALIZER_FACTORY_H
 
 #include <memory>
 #include <pmemcpy/util/errors.h>
-#include <pmemcpy/serialize/msgpack.h>
+/*#include <pmemcpy/serialize/msgpack.h>
 #include <pmemcpy/serialize/cereal.h>
-#include <pmemcpy/serialize/boost.h>
+#include <pmemcpy/serialize/boost.h>*/
 #include <pmemcpy/serialize/capnproto.h>
 #include <pmemcpy/serialize/capnproto_nocompress.h>
 #include <pmemcpy/serialize/no_serializer.h>
@@ -17,9 +18,9 @@
 namespace pmemcpy {
 
 enum class SerializerType {
-    CEREAL,
+    /*CEREAL,
     MSGPACK,
-    BOOST,
+    BOOST,*/
     CAPNPROTO,
     CAPNPROTO_NOCOMPRESS,
     NO_SERIALIZER
@@ -28,9 +29,9 @@ enum class SerializerType {
 class SerializerTypeConverter {
 public:
     static SerializerType convert(std::string name) {
-        if(name == "CEREAL") return SerializerType::CEREAL;
+        /*if(name == "CEREAL") return SerializerType::CEREAL;
         if(name == "MSGPACK") return SerializerType::MSGPACK;
-        if(name == "BOOST") return SerializerType::BOOST;
+        if(name == "BOOST") return SerializerType::BOOST;*/
         if(name == "CAPNPROTO") return SerializerType::CAPNPROTO;
         if(name == "CAPNPROTO_NOCOMPRESS") return SerializerType::CAPNPROTO_NOCOMPRESS;
         if(name == "NO_SERIALIZER") return SerializerType::NO_SERIALIZER;
@@ -38,9 +39,9 @@ public:
     }
     static std::string convert(SerializerType id) {
         switch(id) {
-            case SerializerType::CEREAL: return "CEREAL";
+            /*case SerializerType::CEREAL: return "CEREAL";
             case SerializerType::MSGPACK: return "MSGPACK";
-            case SerializerType::BOOST: return "BOOST";
+            case SerializerType::BOOST: return "BOOST";*/
             case SerializerType::CAPNPROTO: return "CAPNPROTO";
             case SerializerType::CAPNPROTO_NOCOMPRESS: return "CAPNPROTO_NOCOMPRESS";
             case SerializerType::NO_SERIALIZER: return "NO_SERIALIZER";
@@ -53,9 +54,9 @@ class SerializerFactory {
 public:
     static std::unique_ptr<Serializer<T>> get(SerializerType type) {
         switch(type) {
-            case SerializerType::CEREAL: { return std::unique_ptr<CerealSerializer<T>>(new CerealSerializer<T>()); }
+            /*case SerializerType::CEREAL: { return std::unique_ptr<CerealSerializer<T>>(new CerealSerializer<T>()); }
             case SerializerType::MSGPACK: { return std::unique_ptr<MsgpackSerializer<T>>(new MsgpackSerializer<T>()); }
-            case SerializerType::BOOST: { return std::unique_ptr<BoostSerializer<T>>(new BoostSerializer<T>()); }
+            case SerializerType::BOOST: { return std::unique_ptr<BoostSerializer<T>>(new BoostSerializer<T>()); }*/
             case SerializerType::CAPNPROTO: { return std::unique_ptr<CapnProtoSerializer<T>>(new CapnProtoSerializer<T>()); }
             case SerializerType::CAPNPROTO_NOCOMPRESS: return std::unique_ptr<CapnProtoNoCompressSerializer<T>>(new CapnProtoNoCompressSerializer<T>());
             case SerializerType::NO_SERIALIZER: return std::unique_ptr<NoSerializer<T>>(new NoSerializer<T>());
--- readme.md
+++ readme.md
@@ -6,16 +6,74 @@ memory devices.
 
 ## Dependencies
 
-* cereal, msgpack, boost, capnproto
+* boost, capnproto
 * pmdk
-* netcdf
-* adios
 
 ## Install
 
-```{bash}
+```bash
 mkdir build
 cd build
 cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
-cmake ../ -DCMAKE_INSTALL_PREFIX=`scspkg pkg-root pmemcpy`
+```
+
+To build with tests (requires netcdf, adios, and pnetcdf):
+```bash
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_TESTS
+```
+
+## Usage
+
+Examples can be seen under test/unit and test/performance.
+test/api was used to make pseudocode for the paper, and differs slightly from the actual work.
+
+### APIs
+```C
+#include <pmemcpy/memcpy.h>
+
+pmemcpy::PMEM pmem(StorageType storage, SerializerType serializer, bool use_mmap);
+pmem.mmap(std::string path, size_t size);
+pmem.munmap()
+
+pmem.store<T>(std::string id, T &data);
+pmem.store<T>(std::string id, T *data, pmemcpy::Dimensions dims);
+
+pmem.load<T>(std::string id, T &data);
+pmem.load<T>(std::string id, T *data, pmemcpy::Dimensions dims);
+
+pmem.free(std::string id);
+```
+
+* pmem.mmap: Allocates "size" bytes from the file at "path". Creates the file with this size if it does not exist.
+* pmem.munmap: Unmap the memory region. Automatically called in destructor.
+
+## Limitations
+
+* pmem.mmap must be called with the "size" parameter in order to create the pool for the PMDK storage type. PMDK pools cannot be extended whence allocated. However, the size parameter is not needed for the POSIX storage type; size is determined automatically.
+* pmem.mmap cannot be called in parallel to the same path when using PMDK. In other words, when using PMDK with MPI, you must either share the PMEM object among all processes or use file-per-processs. The POSIX storage type does not have this limitation.
+* For writes, CapnProto requires the memory region where data gets serialized to be zeroed using memset, which is fairly expensive. Reads are fine.
+* CapnProto can only serialize primitive types (char, int, float, double) and C-style arrays of these types. Slightly more work would have to be done in order to incorporate STL or custom structured types.
+
+### Serializer Types
+Different serialization methods can be used to store the data in PMEM:
+
+```C
+SerializerType::CAPNPROTO
+SerializerType::CAPNPROTO_NOCOMPRESS
+SerializerType::NO_SERIALIZER
+```
+
+By default, CapnProto will encode data using a simple deduplication algorithm. This can be disabled using the CAPNPROTO_NOCOMPRESS option.
+
+### Storage Types
+The act of placing data into the PMEM can be done using either POSIX or PMDK.
+The POSIX method will assume that a directory is passed to pmem.mmap, and each
+variable created using pmem.store will be a file in this directory. You cannot
+use "/" in the id name.
+
+```C
+StorageType::POSIX
+StorageType::PMDK
 ```
